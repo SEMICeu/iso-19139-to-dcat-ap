dc426fee29889c2f3c4899af76a123727524572e
PEREGO Andrea <andrea.perego@jrc.ec.europa.eu>
2015-08-07 15:43:44 +0200

    Initial commit

---
+++ api/README.md
@@ -0,0 +1,80 @@
+﻿# GeoDCAT-AP API (GeoDCAT-API)
+
+This API is a proof-of-concept of the implementation of GeoDCAT-AP in an [OGC CSW (Catalog Service for the Web)](http://www.opengeospatial.org/standards/cat), re-using the standard CSW interface, and supporting in addition [HTTP content negotiation](https://tools.ietf.org/html/rfc7231#section-3.4).
+
+More precisely, GeoDCAT-API uses the standard CSW parameters `outputSchema` and `outputFormat` to determine, respectively, (a) the GeoDCAT-AP profile to be used (core or extended), and (b) the RDF serialisation to be returned.
+
+The ISO 19139 records to be transformed is specified by a GeoDCAT-API-specific parameter `src`, which is not part of the CSW interface. 
+
+The API uses the GeoDCAT-AP XSLT to transform ISO 19139 records into GeoDCAT-AP. As such, the API works both on static files including the records, and on the CSW output of a `GetRecords` or `GetRecordById` request.
+
+A working demo of GeoDCAT-API is available at: 
+
+http://geodcat-ap.semic.eu:8890/geodcat-api/
+
+## API specification
+
+### Supported HTTP methods
+
+The current version of GeoDCAT-API supports only the HTTP `GET` method. As a consequence, it can used only on CSWs supporting `GET` requests.
+
+### API parameters
+
+<table>
+  <thead>
+    <tr>
+      <th>Parameter</th>
+      <th>Description</th>
+      <th>Possible values</th>
+      <th>Default value</th>
+      <th>Notes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><code>ouputSchema</code></td>
+      <td>The GeoDCAT-AP profile to be used for the transformation</td>
+      <td><code>core</code> (DCAT-AP), <code>extended</code> (GeoDCAT-AP)</td>
+      <td><code>core</code></td>
+      <td>
+        <p>If this parameter is omitted, the API uses the "core" profile as default.</p>
+        <p>The "core" profile is labelled "DCAT-AP", since it returns just the metadata elements supported in DCAT-AP.</p>
+      </td>
+    </tr>
+    <tr>
+      <td><code>outputFormat</code></td>
+      <td>The RDF serialisation to be returned</td>
+      <td><code>application/rdf+xml</code>, <code>text/turtle</code>, <code>text/n3</code>, <code>application/n-triples</code>, <code>application/ld+json</code></td>
+      <td>N/A</td>
+      <td>If this parameter is omitted, the returned RDF serialisation is determined via HTTP content negotiation</td>
+    </tr>
+    <tr>
+      <td><code>src</code></td>
+      <td>The URL of the resource containing the ISO 19139 records to be tranformed</td>
+      <td>A URL</td>
+      <td>N/A</td>
+      <td><strong>NB</strong>: <em>This is an internal parameter of the GeoDCAT-API</em></td>
+    </tr>
+  </tbody>
+</table>
+
+## Implementation details
+
+GeoDCAT-API is implemented in [PHP5](http://php.net/), and run on top of an [Apache 2 HTTP server](http://httpd.apache.org/).
+
+The [EasyRDF](http://www.easyrdf.org/) and the [ML/JSON-LD](https://github.com/lanthaler/JsonLD) PHP libraries are used to generate the supported RDF serialisations.
+
+## Installation instructions
+
+GeoDCAT-API have been tested on both Linux and Windows, with Apache 2 and PHP 5.3.2 (or later) installed and running.
+
+**NB**: GeoDCAT-API makes use of the [PHP XSL extension](http://php.net/manual/en/xsl.installation.php).
+
+The repository includes all what is necessary, with the exception of EasyRDF and ML/JSON-LD], that must be installed separately by using [Composer](https://getcomposer.org/).
+
+More precisely:
+* Go to folder [`./lib/composer/`](./lib/composer/).
+* [Download Composer](https://getcomposer.org/download/). E.g.: `curl -s https://getcomposer.org/installer | php`
+* Run `php composer.phar install`
+
+You will now be able to run the API from a Web folder.
---
+++ api/index.php
@@ -0,0 +1,230 @@
+<?php
+
+// The API
+
+  $apiSrcRep = "https://webgate.ec.europa.eu/CITnet/stash/projects/ODCKAN/repos/iso-19139-to-dcat-ap/api";
+
+  require('./lib/composer/vendor/autoload.php');
+
+  $outputSchemas = array();
+  $outputSchemas['core'] = array(
+    'label' => 'DCAT-AP',
+    'description' => 'TBD',
+    'url' => 'TBD',
+    'xslt' => 'https://webgate.ec.europa.eu/CITnet/stash/projects/ODCKAN/repos/iso-19139-to-dcat-ap/browse/iso-19139-to-dcat-ap.xsl?raw',
+    'params' => array(
+      'profile' => 'core'
+    )
+  );
+  $outputSchemas['extended'] = array(
+    'label' => 'GeoDCAT-AP',
+    'description' => 'TBD',
+    'url' => 'TBD',
+    'xslt' => 'https://webgate.ec.europa.eu/CITnet/stash/projects/ODCKAN/repos/iso-19139-to-dcat-ap/browse/iso-19139-to-dcat-ap.xsl?raw',
+    'params' => array(
+      'profile' => 'extended'
+    )
+  );
+  $defaultOutputSchema = 'core';
+
+
+  $outputFormats = array();
+//  $outputFormats['text/html'] = array('HTML+RDFa','');
+  $outputFormats['application/rdf+xml'] = array('RDF/XML','rdf');
+  $outputFormats['text/turtle'] = array('Turtle','turtle');
+  $outputFormats['text/n3'] = array('N3','n3');
+  $outputFormats['application/n-triples'] = array('N-Triples','ntriples');
+  $outputFormats['application/ld+json'] = array('JSON-LD','jsonld');
+  $defaultOutputFormat = 'application/rdf+xml';
+
+  function returnHttpError($code) {
+    $title =  $_SERVER["SERVER_PROTOCOL"] . ' ' . $code;
+    $content = '';
+    switch ($code) {
+      case 300:
+        $title .= ' Multiple Choices';
+        $content = '';
+        break;
+      case 404:
+        $title .= ' Not Found';
+        $content = 'The requested URL <code>' . $_SERVER["REQUEST_URI"] . '</code> was not found on this server.';
+        break;
+      case 415:
+        $title .= ' Unsupported Media Type';
+        $content = 'The server does not support the media type transmitted in the request.';
+        break;
+    }
+    http_response_code($code);
+    echo '<!DOCTYPE html><html><head><title>' . $title . '</title></head><body><h1>' . $title . '</h1><p>' . $content . '</p></body></html>';
+    exit;
+  }
+
+
+  if (isset($_GET['src'])) {
+
+    $xmluri = $_GET['src'];
+
+    $outputSchema = $outputSchemas[$defaultOutputSchema];
+    $xsluri = $outputSchema['xslt'];
+    if (isset($_GET['outputSchema'])) {
+      if (isset($outputSchemas[$_GET['outputSchema']])) {
+        $outputSchema = $_GET['outputSchema'];
+        $xsluri = $outputSchemas[$outputSchema]['xslt'];
+      }
+      else {
+        returnHttpError(404);
+      }
+    }
+
+    $xml = new DOMDocument;
+    if (!$xml->load($xmluri)) {
+      returnHttpError(404);
+    }
+
+    $xsl = new DOMDocument;
+    if (!$xsl->load($xsluri)) {
+      returnHttpError(404);
+    }
+
+    $proc = new XSLTProcessor();
+    $proc->importStyleSheet($xsl);
+
+    foreach ($outputSchemas[$outputSchema]['params'] as $k => $v) {
+      $proc->setParameter("", $k, $v);
+    }
+
+    if (!$rdf = $proc->transformToXML($xml)) {
+      returnHttpError(404);
+    }
+
+    $rdfxmlURL = 'http://' . $_SERVER["SERVER_NAME"] . str_replace($_SERVER["QUERY_STRING"], '', $_SERVER["REQUEST_URI"]) . 'outputSchema=' . rawurlencode($outputSchema) . '&outputFormat=' . rawurlencode("application/rdf+xml") . '&src=' . rawurlencode($_GET['src']);
+
+    $outputFormat = $defaultOutputFormat;
+    if (isset($_GET['outputFormat'])) {
+      if (!isset($outputFormats[$_GET['outputFormat']])) {
+        returnHttpError(415);
+      }
+      else {
+        $outputFormat = $_GET['outputFormat'];
+      }
+    }
+    else {
+      preg_match_all("/[a-zA-Z0-9]+\/[a-zA-Z0-9\!\#\$\&\-\^_\.\+]+/",$_SERVER['HTTP_ACCEPT'],$matches);
+      if (count($matches[0]) == 0) {
+        returnHttpError(415);
+      }
+      else {
+        $acceptedFormats = $matches[0];
+        $supportedFormats = array_keys($outputFormats);
+        $candidateFormats = array_intersect($supportedFormats, $acceptedFormats);
+        switch (count($candidateFormats)) {
+          case 0:
+            returnHttpError(415);
+            break;
+          case 1:
+            $outputFormat = $candidateFormats[0];
+            break;
+          default:
+            returnHttpError(300);
+            break;
+        }
+      }
+    }
+
+    EasyRdf_Namespace::set('dc', 'http://purl.org/dc/elements/1.1/');
+    EasyRdf_Namespace::set('locn', 'http://www.w3.org/ns/locn#');
+    EasyRdf_Namespace::set('adms', 'http://www.w3.org/ns/adms#');
+    EasyRdf_Namespace::set('cnt', 'http://www.w3.org/2011/content#');
+    $graph = new EasyRdf_Graph;
+    $graph->parse($rdf, "rdfxml", $rdfxmlURL);
+
+    header("Content-type: " . $outputFormat);
+    echo $graph->serialise($outputFormats[$outputFormat][1]);
+    exit;
+/*
+    if ($outputFormat == 'text/html') {
+      $xml = new DOMDocument;
+      $xml->loadXML($rdf) or die();
+      $xsl = new DOMDocument;
+      $xsl->load("");
+      $proc = new XSLTProcessor();
+      $proc->importStyleSheet($xsl);
+      echo $proc->transformToXML($xml);
+      exit;
+    }
+    else {
+      echo $graph->serialise($outputFormats[$outputFormat][1]);
+      exit;
+    }
+*/
+  }
+
+// Variables for API landing page.
+
+  $title = "GeoDCAT-AP API";
+  $exampleSrcURL = "";
+  $footer = '<p>' . $title . ' @ Stash: <a href="' . $apiSrcRep . '">' . $apiSrcRep . '</a></p>';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  </head>
+  <body>
+    <header><h1><?php echo $title; ?></h1></header>
+    <nav>
+    </nav>
+    <section id="input-box">
+      <form id="api" action="." method="get">
+        <h1>
+          <label for="outputSchema">Output Schema : </label>
+          <select id="outputSchema" name="outputSchema">
+<?php
+
+  foreach ($outputSchemas as $k => $v) {
+    echo '            <option value="' . $k . '">' . $v['label'] . '</option>' . "\n";
+  }
+
+?>
+          </select>
+          <span style="float:right">
+            <input type="submit" id="transform" value="Transform"/>
+          </span>
+        </h1>
+        <textarea id="src" name="src" title="Copy &amp; paste the URL of ISO 19139 records" placeholder="Copy &amp; paste the URL of ISO 19139 records"><?php echo $exampleSrcURL?></textarea>
+        <div style="height:2em">
+          <span style="float:right;">
+            <label for="outputFormat">Output format : </label>
+            <select id="outputFormat" name="outputFormat">
+<?php
+
+  foreach ($outputFormats as $k => $v) {
+    $selected = '';
+    if ($k == $defaultOutputFormat) {
+      $selected = ' selected="selected"';
+    }
+    echo '              <option value="' . $k . '"' . $selected . '>' . $v[0] . '</option>' . "\n";
+  }
+
+?>
+            </select>
+          </span>
+        </div>
+      </form>
+      <section>
+        <h3>Usage notes</h3>
+        <p>Copy &amp; paste the URL of a file or of a CSW request returning ISO 19139 records.</p>
+        <p>Supported CSW request types: <code>GetRecords</code>, <code>GetRecordById</code>.</p>
+        <p>Supported CSW output schema: <code>http://www.isotc211.org/2005/gmd</code></p>
+        <p><strong>NB</strong>: The current version of the API supports only CSW calls using the <code>GET</code> HTTP method.</p>
+        <p><em>A description of the GeoDCAT-AP API is available on the <a href="<?php echo $apiSrcRep; ?>">API's Stash repository</a>.</em></p>
+      </section>
+    </section>
+    <aside>
+    </aside>
+    <footer><?php echo $footer; ?></footer>
+  </body>
+</html>
