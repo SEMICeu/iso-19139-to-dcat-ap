acae4e0845d1e3c483b4ce4098f88a909ce847fe
Andrea Perego <andrea-perego@users.noreply.github.com>
2021-01-06 23:23:49 +0100

    Add new namespace declarations (#21)
    Add namespace declarations for vocabularies used in GeoDCAT-AP version 2.0.0.

--- api/index.php
+++ api/index.php
@@ -199,17 +199,23 @@
     }
 
 // Setting namespace prefixes
-    \EasyRdf\RdfNamespace::set('adms', 'http://www.w3.org/ns/adms#');
-    \EasyRdf\RdfNamespace::set('cnt', 'http://www.w3.org/2011/content#');
-    \EasyRdf\RdfNamespace::set('dc', 'http://purl.org/dc/elements/1.1/');
-    \EasyRdf\RdfNamespace::set('dcat', 'http://www.w3.org/ns/dcat#');
-    \EasyRdf\RdfNamespace::set('gsp', 'http://www.opengis.net/ont/geosparql#');
-    \EasyRdf\RdfNamespace::set('locn', 'http://www.w3.org/ns/locn#');
-    \EasyRdf\RdfNamespace::set('prov', 'http://www.w3.org/ns/prov#');
+
+    \EasyRdf\Namespace::set('adms', 'http://www.w3.org/ns/adms#');
+    \EasyRdf\Namespace::set('cnt', 'http://www.w3.org/2011/content#');
+    \EasyRdf\Namespace::set('dc', 'http://purl.org/dc/elements/1.1/');
+    \EasyRdf\Namespace::set('dcat', 'http://www.w3.org/ns/dcat#');
+    \EasyRdf\Namespace::set('dqv', 'http://www.w3.org/ns/dqv#');
+    \EasyRdf\Namespace::set('geodcatap', 'http://data.europa.eu/930/');
+    \EasyRdf\Namespace::set('gsp', 'http://www.opengis.net/ont/geosparql#');
+    \EasyRdf\Namespace::set('locn', 'http://www.w3.org/ns/locn#');
+    \EasyRdf\Namespace::set('prov', 'http://www.w3.org/ns/prov#');
+//** EasyRdf requires prefixes to contain only alpha-numeric chars - see https://github.com/easyrdf/easyrdf/issues/265
+//    \EasyRdf\RdfNamespace::set('sdmx-attribute', 'http://purl.org/linked-data/sdmx/2009/attribute#');
 
 // Creating the RDF graph from the RDF/XML serialisation
 
-    $graph = new \EasyRdf\Graph();
+    $graph = new \EasyRdf\Graph;
+
     $graph->parse($rdf);
 
 // Sending HTTP headers
